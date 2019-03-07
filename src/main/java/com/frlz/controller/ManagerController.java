2752f9362ed5fc4b32e766f1af5baf9be565ddee
zs <845053535@qq.com>
2019-03-07 11:00:07 +0800

    20190307

--- src/main/java/com/frlz/controller/ManagerController.java
+++ src/main/java/com/frlz/controller/ManagerController.java
@@ -1,4 +1,37 @@
 package com.frlz.controller;
 
+import com.frlz.pojo.User;
+import com.frlz.service.UserService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 public class ManagerController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/managerLogin")
+    public String managerLogin(@Param("manager") String manager, @Param("password") String password){
+        if ("managercz".equals(manager)){
+            if ("123".equals(password)){
+                return "success";
+            }
+            return "password is wrong";
+        }
+        return "manager not exist";
+    }
+
+    @RequestMapping("/deleteUser")
+    public String deleteUser(String uid){
+        userService.deleteUserByUid(uid);
+        return "success";
+    }
+
+    @RequestMapping("/selectAllUser")
+    public List<User> selectAllUser(){
+
+        return userService.getAll();
+    }
 }
