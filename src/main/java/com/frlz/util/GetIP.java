bbf3169c48401799c2c82332f2a9a871adb6c462
zs <845053535@qq.com>
2019-03-15 15:40:49 +0800

    201903111710

--- src/main/java/com/frlz/util/GetIP.java
+++ src/main/java/com/frlz/util/GetIP.java
@@ -3,22 +3,13 @@ package com.frlz.util;
 import javax.servlet.http.HttpServletRequest;
 
 public class GetIP {
-    public String getIp(HttpServletRequest request)  throws Exception{
-        if (request == null) {
-            throw (new Exception("getIpAddr method HttpServletRequest Object is null"));
+    public String getIp(HttpServletRequest request)  {
+        if (request.getHeader("x-forwarded-for") == null) {
+            return request.getRemoteAddr();
         }
-        String ipString = request.getHeader("x-forwarded-for");
-        if (ipString == null ? true : ipString.trim().length() == 0 || "unknown".equalsIgnoreCase(ipString)) {
-            ipString = request.getHeader("Proxy-Client-IP");
-        }
-        if (ipString == null ? true : ipString.trim().length() == 0  || "unknown".equalsIgnoreCase(ipString)) {
-            ipString = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ipString == null ? true : ipString.trim().length() == 0  || "unknown".equalsIgnoreCase(ipString)) {
-            ipString = request.getRemoteAddr();
-        }
+        return request.getHeader("x-forwarded-for");
 
-        // 多个路由时，取第一个非unknown的ip
+        /*// 多个路由时，取第一个非unknown的ip
         final String[] arr = ipString.split(",");
         for (final String str : arr) {
             if (!"unknown".equalsIgnoreCase(str)) {
@@ -27,7 +18,7 @@ public class GetIP {
             }
         }
 
-        return ipString;
+        return ipString;*/
 
 
     }
