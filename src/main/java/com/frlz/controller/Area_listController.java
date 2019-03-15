3d5c569b44a5531cc2a50f7aa5e0812917c9cb8a
zs <845053535@qq.com>
2019-03-15 14:48:05 +0800

    201903111710

--- src/main/java/com/frlz/controller/Area_listController.java
+++ src/main/java/com/frlz/controller/Area_listController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 public class Area_listController{
     @RequestMapping("/getip")
-    public String getip(HttpServletRequest request)  {
+    public String getip(HttpServletRequest request) throws Exception {
         GetIP getIP = new GetIP();
         return getIP.getIp(request) ;
     }
--- src/main/java/com/frlz/util/GetIP.java
+++ src/main/java/com/frlz/util/GetIP.java
@@ -1,37 +1,35 @@
 package com.frlz.util;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
 
 public class GetIP {
-    public String getIp(HttpServletRequest request)  {
-        String ipAddress = request.getHeader("x-forwarded-for");
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("Proxy-Client-IP");
+    public String getIp(HttpServletRequest request)  throws Exception{
+        if (request == null) {
+            throw (new Exception("getIpAddr method HttpServletRequest Object is null"));
         }
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        String ipString = request.getHeader("x-forwarded-for");
+        if (ipString == null ? true : ipString.trim().length() == 0 || "unknown".equalsIgnoreCase(ipString)) {
+            ipString = request.getHeader("Proxy-Client-IP");
         }
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getRemoteAddr();
-            if(ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")){
-                //根据网卡取本机配置的IP
-                InetAddress inet=null;
-                try {
-                    inet = InetAddress.getLocalHost();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                ipAddress= inet.getHostAddress();
+        if (ipString == null ? true : ipString.trim().length() == 0  || "unknown".equalsIgnoreCase(ipString)) {
+            ipString = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipString == null ? true : ipString.trim().length() == 0  || "unknown".equalsIgnoreCase(ipString)) {
+            ipString = request.getRemoteAddr();
+        }
+
+        // 多个路由时，取第一个非unknown的ip
+        final String[] arr = ipString.split(",");
+        for (final String str : arr) {
+            if (!"unknown".equalsIgnoreCase(str)) {
+                ipString = str;
+                break;
             }
         }
-        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-        if(ipAddress!=null && ipAddress.length()>15){ //"***.***.***.***".length() = 15
-            if(ipAddress.indexOf(",")>0){
-                ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));
-            }
-        }
-        return ipAddress;
+
+        return ipString;
+
+
     }
 
 
