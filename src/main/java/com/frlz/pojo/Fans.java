5a99b26eb6881cf10e31e585c99b41878fca3a7a
zs <845053535@qq.com>
2019-03-16 14:46:57 +0800

    201903111710

---
+++ src/main/java/com/frlz/pojo/Fans.java
@@ -0,0 +1,10 @@
+package com.frlz.pojo;
+
+import lombok.Data;
+
+@Data
+public class Fans {
+    private int fansId;//
+    private String uid;//被粉的人的uid
+    private String fansUId;//粉丝的uid
+}
---
+++ src/main/java/com/frlz/service/FansService.java
@@ -0,0 +1,12 @@
+package com.frlz.service;
+
+import com.frlz.pojo.Fans;
+
+import java.util.List;
+
+public interface FansService {
+
+    void insertFans(String uid,String fansUId);
+
+    List<Fans> selectFansForOne(String uid);
+}
