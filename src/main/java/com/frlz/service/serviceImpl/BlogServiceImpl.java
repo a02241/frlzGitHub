c415e318e13ba0b65b12b8caa509716fe5876b20
cz <b02241@163.com>
2019-03-20 17:14:16 +0800

    权重

--- src/main/java/com/frlz/service/serviceImpl/BlogServiceImpl.java
+++ src/main/java/com/frlz/service/serviceImpl/BlogServiceImpl.java
@@ -87,7 +87,7 @@ public class BlogServiceImpl implements BlogService {
         int count = blogMapper.selectBlogCountByDateAndUid(format2,uid);
         blog.setTime(date);
         blog.setLikes(0);
-        blog.setWeight(0);
+        blog.setWeight(50);
         blog.setCommentsNumber(0);
         blog.setForwordNumber(0);
         blog.setReadNumber(0);
