18699871ab00f7738351a7bf8d2bd25cf0cb27f2
cz <b02241@163.com>
2019-03-28 10:08:30 +0800

    更新股票

---
+++ src/main/java/com/frlz/utilPojo/UtilTradeLog.java
@@ -0,0 +1,21 @@
+package com.frlz.utilPojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @program: frlz
+ * @description: 记录交易时间
+ * @author: cz
+ * @date: 2019-03-07 15:16
+ **/
+@Data
+public class UtilTradeLog {
+    private Date tradeTime;//交易时间
+    private String balanceId;//外键
+    private int tradeQuantum;//量子交易
+    private int tradeBlock;//方块交易
+    private int tradeMagicCube;//魔方交易
+    private String remarks;//备注
+}
