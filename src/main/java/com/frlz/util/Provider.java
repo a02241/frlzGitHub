88c6a80972c0cabb00f3a7bfe45290aaa17454d9
cz <b02241@163.com>
2019-03-30 15:43:26 +0800

    修改

--- src/main/java/com/frlz/util/Provider.java
+++ src/main/java/com/frlz/util/Provider.java
@@ -3,13 +3,13 @@ package com.frlz.util;
 public class Provider {
     public String queryShares(String code ,String before ,String end){
         StringBuilder sb = new StringBuilder();
-        sb.append("select * from `" + code + "`WHERE date BETWEEN cast('"+before+"' as datetime) AND cast('"+end+"' as datetime)");
+        sb.append("select * from `" + code + "`WHERE date BETWEEN cast('"+before+"' as datetime) AND cast('"+end+"' as datetime) ORDER BY str_to_date(date, '%Y-%m-%d') desc");
         return sb.toString();
     }
 
     public String querySharesCount(String code ,String before ,String end){
         StringBuilder sb = new StringBuilder();
-        sb.append("select count(low) from `" + code + "`WHERE date BETWEEN cast('"+before+"' as datetime) AND cast('"+end+"' as datetime)" );
+        sb.append("select count(low) from `" + code + "`WHERE date BETWEEN cast('"+before+"' as datetime) AND cast('"+end+"' as datetime) ORDER BY str_to_date(date, '%Y-%m-%d') desc" );
         return sb.toString();
     }
 }
