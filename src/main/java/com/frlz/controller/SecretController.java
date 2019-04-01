686bf6dcf8100482c83c4291ff27411943630ceb
cz <b02241@163.com>
2019-04-01 13:54:55 +0800

    修改

--- src/main/java/com/frlz/controller/SecretController.java
+++ src/main/java/com/frlz/controller/SecretController.java
@@ -57,7 +57,22 @@ public class SecretController {
         return R.isOk().data(secretService.selectFromSecret(uid));
     }
 
-    @PostMapping("/checkSecret")//传入uid以及三个答案，返回int类型，个位为1则第一题正确，十位为1则第二题正确，百位为1则第三题正确
+    @PostMapping("/checkSecret")
+    /**
+     * TODO 验证密保正确性
+     * @title checkSecret
+     * @create by: cz
+     * @description: TODO 必填参数 answer1,answer2,answer3,uid
+     * @create time: 2019/4/1 9:26
+     * @Param: answer1
+     * @Param: answer2
+     * @Param: answer3
+     * @Param: uid
+     * @throws 
+     * @return com.frlz.util.R<java.lang.Integer>
+     * @version V1.0
+     */
+    //传入uid以及三个答案，返回int类型，个位为1则第一题正确，十位为1则第二题正确，百位为1则第三题正确
     public R<Integer> checkSecret(String answer1,String answer2,String answer3,String uid){
         return R.isOk().data(secretService.checkSecret(uid,answer1,answer2,answer3));
     }
