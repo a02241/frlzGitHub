e7c1fec52eb9ade7bef70120ddf353e5fcdeb3c5
zs <845053535@qq.com>
2019-04-08 16:49:00 +0800

    20190408

---
+++ src/main/java/com/frlz/util/GetIP.java
@@ -0,0 +1,30 @@
+package com.frlz.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class GetIP {
+    public static String getip(HttpServletRequest request){
+        String remoteAddr = request.getRemoteAddr();
+        String forwarded = request.getHeader("X-Forwarded-For");
+        String realIp = request.getHeader("X-Real-IP");
+
+        String ip;
+        if (realIp == null) {
+            if (forwarded == null) {
+                ip = remoteAddr;
+            } else {
+                ip = remoteAddr + "/" + forwarded.split(",")[0];
+            }
+        } else {
+            if (realIp.equals(forwarded)) {
+                ip = realIp;
+            } else {
+                if(forwarded != null){
+                    forwarded = forwarded.split(",")[0];
+                }
+                ip = realIp + "/" + forwarded;
+            }
+        }
+        return ip;
+    }
+}
