3207eeb7245a416d99262bdc0825a739df0615a0
cz <b02241@163.com>
2019-04-10 10:59:24 +0800

    盘前

--- src/main/java/com/frlz/util/test.java
+++ src/main/java/com/frlz/util/test.java
@@ -2,6 +2,7 @@ package com.frlz.util;
 
 
 import java.util.Date;
+import java.util.Random;
 
 /**
  * @program: frlz
@@ -12,6 +13,24 @@ import java.util.Date;
 
 public class  test {
     public static void main(String[] args) {
-
+        int rand = 0;
+        int maxCount = 0;
+        int minCount = 0;
+        int count = 0;
+        for (int i = 0 ; i <= 9 ; i++){
+            maxCount=i;
+            for (int j = 0 ; j <= 9 ; j++) {
+                minCount=j;
+                for (int g = 0 ; g <= 9 ; g++){
+                    rand=g;
+                    if (maxCount >= 8 && minCount <= 2 && rand > minCount && rand < maxCount) {
+                        count++;
+                        System.out.println(count + "~~~");
+                    }
+                }
+            }
+        }
+        double result = count/ new Double(10*9*8/(3*2));
+        System.out.println(result);
     }
 }
